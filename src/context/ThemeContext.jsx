21c0a32ba0e336225156f2f41454205c582bc8e4
Kesar Karale <kesarkarale1@gmail.com>
2026-05-26 20:06:47 +0530

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,21 +3,25 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [dark, setDark] = useState(() => {
-    return localStorage.getItem("theme") !== "light";
-  });
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "dark"
+  );
 
   useEffect(() => {
-    localStorage.setItem(
-      "theme",
-      dark ? "dark" : "light"
-    );
-  }, [dark]);
+    localStorage.setItem("theme", theme);
+
+    document.body.style.background =
+      theme === "dark" ? "#020617" : "#f8fafc";
+
+    document.body.style.transition = ".3s";
+  }, [theme]);
+
+  function toggleTheme() {
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+  }
 
   return (
-    <ThemeContext.Provider
-      value={{ dark, setDark }}
-    >
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
