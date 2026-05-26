6ca12b3fb4e9f0590f1024475d55ba262bae946a
Kesar Karale <kesarkarale1@gmail.com>
2026-05-26 12:21:32 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,19 +1,35 @@
 export default function Loader() {
   return (
-    <div style={{
-      minHeight:"100vh",
-      display:"grid",
-      placeItems:"center",
-      background:"#020617"
-    }}>
-      <div style={{
-        width:70,
-        height:70,
-        border:"6px solid rgba(255,255,255,.1)",
-        borderTop:"6px solid #22d3ee",
-        borderRadius:"50%",
-        animation:"spin 1s linear infinite"
-      }} />
-    </div>
+    <>
+      <style>
+        {`
+          @keyframes spin {
+            to {
+              transform: rotate(360deg);
+            }
+          }
+        `}
+      </style>
+
+      <div
+        style={{
+          minHeight: "100vh",
+          display: "grid",
+          placeItems: "center",
+          background: "#020617"
+        }}
+      >
+        <div
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: "50%",
+            border: "6px solid rgba(255,255,255,.14)",
+            borderTop: "6px solid #22d3ee",
+            animation: "spin 1s linear infinite"
+          }}
+        />
+      </div>
+    </>
   );
 }
